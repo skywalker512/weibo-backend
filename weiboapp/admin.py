8f75da94d3e26a81d80b1625d5ad9e3c06d6d5bb
skywalker512 <houzhenhong@hotmail.com>
2019-02-09 14:56:41 +0800

    [修改] 后端使用 koa2 + mongodb 的组合，django 学习曲线太抖零

--- weiboapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from weiboapp.models import Link, Article, Tag, UserProfile
-# Register your models here.
-
-admin.site.register(Link)
-admin.site.register(Article)
-admin.site.register(Tag)
-admin.site.register(UserProfile)
