2519787d5f70671b0be4fe1e4245804a0c712def
skywalker512 <houzhenhong@hotmail.com>
2019-02-10 20:21:23 +0800

    [fix] 添加 ref

--- models/article/article.js
+++ models/article/article.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
     title: { type: String, required: true },
-    categoryId: { type: Schema.Types.ObjectId, required: true }, // ref: 'Catagory' ??
+    categoryId: { type: Schema.Types.ObjectId, required: true, ref: 'Catagory' }, // ref: 'Catagory' ?? 在使用 populate 的时候必须使用 ref
     cover: { type: String, default: null },
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    authorId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
     review: { type: Number, default: 0 },
     praise: { num: Number, user: Array }, // 这里应该要考虑性能问题，不使用数据库查询总个数而是直接统计
     content: { type: String, required: true },
