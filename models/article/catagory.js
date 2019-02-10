033253909808492e3716fc51483847173ca45646
skywalker512 <houzhenhong@hotmail.com>
2019-02-10 20:21:36 +0800

    [修改] 代码风格

--- models/article/catagory.js
+++ models/article/catagory.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const CatagorySchema = new Schema({
-    cate_name: { type: String, default: "" },
+    name: { type: String, default: '' },
     createdAt: { type: Date, default: Date.now },
     lastPublishAt: { type: Date, default: Date.now },  // 最热节点
 })
