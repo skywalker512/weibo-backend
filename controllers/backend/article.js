019472d07af1980905433bd4616b97690edaba79
skywalker512 <houzhenhong@hotmail.com>
2019-02-10 20:20:49 +0800

    [提交] 完成创建分类

--- controllers/backend/article.js
+++ controllers/backend/article.js
@@ -4,10 +4,14 @@ class ArticleController {
     static async createCategory(ctx) {
         if (!ctx.isAuthenticated()) return ctx.error({ msg: '您还没有登陆' });
         if (!ctx.isAdmin()) return ctx.error({ msg: '您没有权限访问' });
-        
-        const { name } = ctx.requset.body;
-        const isExit = await CategoryModel.find({ name })
+
+        const { name } = ctx.request.body;
+        const isExit = await CategoryModel.findOne({ name });
         if( isExit ) return ctx.error({ msg: '分类已存在' });
+        const result = await CategoryModel.create({ name });
+        if( !result ) return ctx.error({ msg: '分类创建失败' });
+
+        return ctx.success({ msg: '创建成功', data: result });
     }
 }
 
