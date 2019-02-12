60540b7160286db2ab1c7ff2a2ea6dead8340613
skywalker512 <houzhenhong@hotmail.com>
2019-02-12 11:31:07 +0800

    [修改] 修改认证方式,减少cookie

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -2,7 +2,8 @@ import md5 from 'md5';
 
 export default async (ctx, next) => {
     ctx.isAuthenticated = () => {
-        if( ctx.cookies.get('userid') && ctx.cookies.get('userid') ===  md5('weibo' + ctx.session.userId) ) {
+        const sym = Object.getOwnPropertySymbols(ctx)[0];
+        if( ctx.cookies.get('weibo') && ctx.cookies.get('weibo') ===  ctx[sym].externalKey) {
             return true;
         } else {
             return false;
@@ -20,14 +21,11 @@ export default async (ctx, next) => {
     ctx.setCookies = ( userId, userGroup ) => {
         ctx.session.userId = userId;
         ctx.session.userGroup = userGroup;
-        const keep_user = 604800000;  // 7天   0 为浏览器进程
-        ctx.cookies.set('userid', md5('weibo' + userId), { maxAge: keep_user, httpOnly: false });
     }
 
     ctx.removeCookies = () => {
         ctx.session.userId = null;
         ctx.session.userGroup = null;
-        ctx.cookies.set('userid', null);
     }
 
     await next();
