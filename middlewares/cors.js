b8ece5811dddf86f8a13b4db43d03d1ec72e9886
skywalker512 <houzhenhong@hotmail.com>
2019-02-12 22:17:17 +0800

    [提交] api 跨域实现

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,5 @@
 export default async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
-
+    ctx.set("Access-Control-Allow-Origin", "http://redrock.test")
+    ctx.set("Access-Control-Allow-Credentials", "true")
     await next();
 }
