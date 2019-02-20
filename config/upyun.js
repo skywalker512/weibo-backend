34c5f61a6577463259e3e527f1e940a8d4c7aa68
skywalker512 <houzhenhong@hotmail.com>
2019-02-20 13:19:30 +0800

    [提交] upyun 的其他信息

--- config/upyun.js
+++ config/upyun.js
@@ -4,6 +4,8 @@ const upyun = {
     bucket: env.UPYUN_BUCKET,
     operator: env.UPYUN_OPERATOR,
     password: env.UPYUN_PASSWORD,
+    contentLengthRange: '1024,12582912',
+    url: env.UPYUN_URL,
 }
 
 export default upyun
--- controllers/frontend/upyun.js
+++ controllers/frontend/upyun.js
@@ -8,8 +8,9 @@ export default class {
     static getHeaderSign(ctx) {
         if (!ctx.isAuthenticated()) return ctx.error({ msg: '您还没有登陆' });
         let params = {}
-        params['save-key'] = '/esotalk/{year}/{mon}/{day}/{hour}_{min}_{sec}_{filename}{.suffix}'
+        params['save-key'] =  ctx.query.path || '/all/{year}/{mon}/{day}/{hour}_{min}_{sec}_{filename}{.suffix}'
+        params['content-length-range'] = upyunConfig.contentLengthRange
         const headSign = upyun.sign.getPolicyAndAuthorization(bucket, params)
-        ctx.success({ data: headSign })
+        ctx.success({ data: {headSign, bucket: upyunConfig.bucket, url: upyunConfig.url} })
     }
 }
