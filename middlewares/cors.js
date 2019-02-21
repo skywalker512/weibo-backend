cda021840fadd57fde996bf4ecee998d6f2ac9a0
skywalker512 <houzhenhong@hotmail.com>
2019-02-21 10:49:11 +0800

    [提交] 前端 url 放在环境变量

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,5 @@
 export default async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', 'http://redrock.test')
+    ctx.set('Access-Control-Allow-Origin', process.env.FE_URL)
     ctx.set('Access-Control-Allow-Credentials', 'true')
     ctx.set('Access-Control-Allow-Methods', 'GET,POST,HEAD,OPTIONS,PUT,PATCH')
     await next();
